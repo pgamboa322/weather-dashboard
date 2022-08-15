262727635041666b841db881fd2f4917a1aa4278
pgamboa322 <pgamboa322@gmail.com>
2022-08-14 23:15:53 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# weather-dashboard
+# Weather Dashboard
 
-User Story
+## User Story
 AS A traveler
 I WANT to see the weather outlook for multiple cities
 SO THAT I can plan a trip accordingly
 
-Acceptance Criteria
+## Acceptance Criteria
 GIVEN a weather dashboard with form inputs
 WHEN I search for a city
 THEN I am presented with current and future conditions for that city and that city is added to the search history
@@ -17,3 +17,12 @@ WHEN I view future weather conditions for that city
 THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 WHEN I click on a city in the search history
 THEN I am again presented with current and future conditions for that city
+
+## Screenshots
+
+![This image shows the code quiz.](./assets/images/screenshot1.png)
+
+## Links
+Deployed link: https://pgamboa322.github.io/weather-dashboard/
+
+Github link: https://github.com/pgamboa322/weather-dashboard
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // .ready makes it not load until something is inputted
 $(document).ready(function () {
-  // create an array for cities to be held
+  
+    // create an array for cities to be held
   let citiesArray = localStorage.getItem("city");
 
   //
@@ -17,32 +18,34 @@ $(document).ready(function () {
     // every entered city from cities array to city button area
     for (let i = 0; i < citiesArray.length; i++) {
     let city = $("<btn>").text(citiesArray[i]);
-      city.addClass("btn btn-info mb-2");
+      city.addClass("btn btn-info mb-2 btn-history");
       cityButtons.prepend(city);
     }
 
-    //   console.log(citiesArray)
   }
 
-  // fetch the api  call for current weather
+  // fetch the api call for current weather
 
   function fetchWeather(searchInput) {
 
     let API = "95d0033c578ce760785239d96ff3f30a";
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=" + API + "&units=imperial";
 
+    //call for current weather
     $.ajax({
-        url: currentWeather,
+        url: weatherURL,
         method: "GET",
       }).then(function (response) {
         $("#currentCity").text(response.name);
-        $("#currentTemp").text("Temp: " + response.main.temp + " &#176; F");
+        $("#currentTemp").text("Temp: " + response.main.temp + " F");
         $("#currentWind").text("Wind Speed: " + response.wind.speed + " mph");
         $("#currentHumidity").text("Humidity: " + response.main.humidity + "%");
+        // $("#currentIcon").attr("src", "http://openweathermap.org/img/wn/" + response.list.weather[0].icon + "@2x.png");
 
         var uvCall = "https://api.openweathermap.org/data/2.5/uvi?appid=" + API + "&lat=" + response.coord.lat + "&lon=" + response.coord.lon;
 
 
+        // call for uv color
         $.ajax({
             url: uvCall,
             method: "GET",
@@ -56,25 +59,70 @@ $(document).ready(function () {
             } else if (index.value > 5 && index.value <= 8) {
               $("#currentUV").addClass(".text-light");
               $("#currentUV").attr("style", "background-color: yellow");
-            } else if (index.value > 9 && index.value <= 10) {
+            } else if (index.value > 9 && index.value <= 13) {
               $("#currentUV").addClass(".text-light");
               $("#currentUV").attr("style", "background-color: red");
             } 
-          });
-
+        });
   });
 
+// call for 5 day weather 
+let weekWeather = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&appid=" + API + "&units=imperial";
+console.log(weekWeather)
+
+//call to five day forecast
+$.ajax({
+  url: weekWeather,
+  method: "GET"
+  }).then(function (weekRes) {
+  console.log(weekRes);
+
+  let list = weekRes.list
+  console.log(list);
+  $("#fiveDaySection").empty()
+
+  //loop for five day forecast variables into cards
+  for (let i = 0; i < list.length; i += 8) {
+
+      let forecastCol = $("<div class='col text-center align-items-center'>");
+      let date = $("<h4 class='card-title'>").text(list[i].dt_txt.split(" ")[0]);
+      let humidity = $("<p class='card-subtitle'>").text("Humidity: " + list[i].main.humidity + " %");
+      let temperature = $("<p class='card-subtitle'>").html("Temp: " + list[i].main.temp + " &#176; F");
+      let windSpeed = $("<p class='card-subtitle'>").html("Wind Speed: " + list[i].wind.speed + " mph");
+      let img = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + list[i].weather[0].icon + "@2x.png");
+
+      forecastCol.append(date);
+      forecastCol.append(temperature);
+      forecastCol.append(humidity);
+      forecastCol.append(img);
+      forecastCol.append(windSpeed)
+      $("#fiveDaySection").append(forecastCol);
+
+  }
+})
 
 }
-
+    // click for city search
   $("#search").click(function () {
     let searchInput = $("#city-input").val();
     citiesArray.push(searchInput);
+    fetchWeather(searchInput);
 
+    // cities in local storage
     localStorage.setItem("city", JSON.stringify(citiesArray));
+
+    console.log(citiesArray)
 
     displayCities();
   });
 
-  // console.log(citiesArray)
+  // show search history in buttons
+
+  $(document).on("click", ".btn-history", function () {
+    let searchInput = $(this).text();
+    fetchWeather(searchInput);
+  });
+
+  displayCities();
+  
 });
